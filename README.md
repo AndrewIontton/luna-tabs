264e2819f00b67bdcb89601749f97d72751e100f
andrew <andrew@93digital.co.uk>
2020-03-06 18:29:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Luna Tabs
+LunaTabs adds basic tabbing functionality for your applications. LunaTabs allows developers to take full control over styling and animations while automatically adding accessibility.
+
+## Example
+There are three required data attributes to setup our tabs. `data-luna-tabs` is used on our wrapper element which contains both our tab buttons and the tabbed content. `data-luna-tab` used on our tab buttons. These require a unique name that relate them to the `data-luna-tab-target` which contains each of our tabbed elements content.
+
+## Basic Markup
+```html
+<div data-luna-tabs>
+
+  <div class="tab-buttons" aria-label="Tabs">
+    <button id="tab-1" type="tab" data-luna-tab="tab-1" aria-controls="panel-1">Tab 1</button>
+    <button id="tab-1" type="tab" data-luna-tab="tab-2" aria-controls="panel-2">Tab 2</button>
+    <button id="tab-1" type="tab" data-luna-tab="tab-3" aria-controls="panel-3">Tab 3</button>
+  </div>
+
+  <div class="tab-container">
+    <div id="panel-1" data-luna-tab-target="tab-1" aria-labelledby="tab-1">Tab Content 1</div>
+    <div id="panel-1" data-luna-tab-target="tab-2" aria-labelledby="tab-2">Tab Content 2</div>
+    <div id="panel-1" data-luna-tab-target="tab-3" aria-labelledby="tab-3">Tab Content 3</div>
+  </div>
+
+</div>
+```
+
+## initialization
+
+1. Firstly install the LunaTabs package via npm.
+
+```bash
+npm install luna-tabs
+```
+
+2. import the class.
+
+```js
+import LunaTabs from 'luna-tabs';
+```
+
+3. Initialize the class.
+
+```js
+// Initialize all tab elements by data attribute.
+const allTabs = document.querySelectorAll('[data-luna-tabs]');
+
+allTabs.forEach(tabs => {
+  const tabContainer = new LunaTabs(tabs);
+});
+
+// Initialize a specific element.
+const tabs = document.querySelector('[data-luna-tabs]');
+const tabContainer = new LunaToggle(tabs);
+```
+
+Toggle will apply an `is-active` class to both the tab button and the tab content element.
+
+A basic reveal for tab content could be as follows:
+
+```scss
+[data-luna-tab-target] {
+  visibility: hidden;
+
+  &.is-active {
+    visibility: visible;
+  }
+}
+```
---
+++ luna-tabs.js
@@ -0,0 +1,90 @@
+export default class LunaTabs {
+  constructor(container) {
+    this.container = container;
+    this.buttons = this.container.querySelectorAll('[data-luna-tab]');
+    this.contents = this.container.querySelectorAll('[data-luna-tab-target]');
+    this.init();
+  }
+  init() {
+    this.setRoles();
+    this.resetTabState();
+    this.setFirstTab();
+    this.clickEvents();
+    this.keyEvents();
+  }
+  setRoles() {
+    this.container.setAttribute('role', 'tablist');
+    this.buttons.forEach(button => {
+      button.setAttribute('role', 'tab');
+      button.setAttribute('tabindex', '-1');
+    });
+    this.contents.forEach(content => {
+      content.setAttribute('role', 'tabpanel');
+      content.setAttribute('tabindex', 0);
+    });
+  }
+  resetTabState() {
+    this.buttons.forEach(button => {
+      button.setAttribute('aria-selected', false);
+      button.setAttribute('tabindex', '-1');
+      button.classList.remove('is-active');
+    });
+    this.contents.forEach(content => {
+      content.setAttribute('aria-hidden', true);
+      content.classList.remove('is-active');
+    });
+  }
+  setFirstTab() {
+    this.buttons[0].setAttribute('aria-selected', true);
+    this.buttons[0].classList.add('is-active');
+    this.buttons[0].setAttribute('tabindex', 0);
+    this.contents[0].setAttribute('aria-hidden', false);
+    this.contents[0].classList.add('is-active');
+  }
+  setActiveTab(button, content) {
+    button.setAttribute('aria-selected', true);
+    button.setAttribute('tabindex', 0);
+    button.classList.add('is-active');
+    content.setAttribute('aria-hidden', false);
+    content.classList.add('is-active');
+  }
+  clickEvents() {
+    this.buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        const tabId = button.getAttribute('data-luna-tab');
+        const content = this.container.querySelector(`[data-luna-tab-target="${tabId}"]`);
+        this.resetTabState();
+        this.setActiveTab(button, content);
+      });
+    });
+  }
+  keyEvents() {
+    const tabList = this.container.querySelector('[role="tablist"]');
+
+    if (tabList === null) {
+      return;
+    }
+
+    let tabFocus = 0;
+
+    tabList.addEventListener('keydown', event => {
+      if (event.keyCode === 39) {
+        tabFocus++;
+        if (tabFocus >= this.buttons.length) {
+          tabFocus = 0;
+        }
+      } else if (event.keyCode === 37) {
+        tabFocus--;
+        if (tabFocus < 0) {
+          tabFocus = this.buttons.length - 1;
+        }
+      }
+      this.buttons.forEach(button => {
+        button.setAttribute('tabindex', '-1');
+        button.blur();
+      });
+      this.buttons[tabFocus].setAttribute('tabindex', 0);
+      this.buttons[tabFocus].focus();
+    });
+  }
+}
